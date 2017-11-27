35d6f896dbceb00f849a36366c9e0b0042d7aef8
Vaishampayan Reddy <vaisham92@gmail.com>
2017-11-26 19:16:05 -0800

    pushing the search changes

--- static/searchController.js
+++ static/searchController.js
@@ -2,5 +2,28 @@
  * Created by Student on 5/18/17.
  */
 wardroberapp.controller('searchController', function ($scope, $http, $location) {
+    $scope.search = function() {
+        var query = {};
+        var queryString = $scope.queryString;
 
+        var school = queryString.split(';');
+        query['school'] = school[0];
+        var interests = school[1].split(',');
+        query['interests'] = interests;
+
+        $http({
+			method : 'POST',
+			url : '/api/interest-based-community',
+			data : query,
+			headers : {
+					'Content-Type' : 'application/json'
+			}
+		}).success(function(data) {
+			if(data.status === 200) {
+				window.location = '/graph';
+			}
+			else {
+			}
+		});
+    };
 });
